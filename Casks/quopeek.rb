f0cbb5dbcab2e1efe5163027665eb861a05aa62f
Luicono <hello@luicono.com>
2026-09-17 19:33:25 -0600

    Track Homebrew 1.0.1 download attempts

--- Casks/quopeek.rb
+++ Casks/quopeek.rb
@@ -2,7 +2,7 @@ cask "quopeek" do
   version "1.0.1"
   sha256 "e2a8fe6045687b972e7c4faaa2e3fabdadae3af566a899b8f00c65dc7d91a9d7"
 
-  url "https://luicono.com/downloads/QuoPeek-#{version}.dmg"
+  url "https://luicono.com/go/homebrew/quopeek/#{version}.dmg"
   name "QuoPeek"
   desc "Menu bar monitor for visible Codex usage"
   homepage "https://luicono.com/"
